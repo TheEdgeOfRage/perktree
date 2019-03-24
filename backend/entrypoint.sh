eab2549914b8caec69b9e62f7c05de41102a9fce
Pavle Portic <pavle.portic@tilda.center>
2019-03-24 20:55:15 +0100

    Create django superuser on startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,6 @@
 
 python parser/parser.py
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}')" | python manage.py shell
 gunicorn -w 4 --bind 0.0.0.0:80 perktree.wsgi:application
 
