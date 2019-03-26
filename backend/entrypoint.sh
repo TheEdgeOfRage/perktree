0c495521bb7fd30a8099473531fd4d3050bdb28f
Pavle Portic <pavle.portic@tilda.center>
2019-03-26 04:50:56 +0100

    Add basic user creation to entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,15 +11,19 @@ python manage.py migrate
 
 echo "
 from django.contrib.auth.models import User as AuthUser;
+from perks.models import User;
 if not AuthUser.objects.filter(username='${DJANGO_ADMIN_USER}').count() :
 	AuthUser.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}')
-" | python manage.py shell
-
-echo "
-from perks.models import User;
-from django.contrib.auth.models import User as AuthUser;
 if not User.objects.filter(base_user__username='${DJANGO_ADMIN_USER}').count() :
-	base_user = AuthUser.objects.get(username='${DJANGO_ADMIN_USER}')
+	base_admin = AuthUser.objects.get(username='${DJANGO_ADMIN_USER}')
+	admin = User(base_user=base_admin)
+	admin.save()
+
+if not AuthUser.objects.filter(username='user').count() :
+	base_user = AuthUser('user', 'user@example.com', 'user')
+	base_user.save()
+if not User.objects.filter(base_user__username='user').count() :
+	base_user = AuthUser.objects.get(username='user')
 	user = User(base_user=base_user)
 	user.save()
 " | python manage.py shell
