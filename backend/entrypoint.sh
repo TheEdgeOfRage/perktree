cef3b1ae8bcbec4d76c44b0f1518d4d7e80119c6
Pavle Portic <pavle.portic@tilda.center>
2019-03-26 04:38:05 +0100

    Fix entrypoint indentation and add checks for existing users

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,11 +8,13 @@
 
 sleep 5
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}')" | python manage.py shell
-echo " \
-	from perks.models import User; \
-	from django.contrib.auth.models import User as AuthUser; \
-	base_user = AuthUser.objects.get(id=1) \
+echo "
+from perks.models import User; \
+from django.contrib.auth.models import User as AuthUser; \
+if(not AuthUser.objects.filter(username='${DJANGO_ADMIN_USER}').count()): \
+	AuthUser.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}') \
+if(not User.objects.filter(base_user__username='${DJANGO_ADMIN_USER}').count()): \
+	base_user = AuthUser.objects.get(username='${DJANGO_ADMIN_USER}') \
 	user = User(base_user=base_user) \
 	user.save()" | python manage.py shell
 
