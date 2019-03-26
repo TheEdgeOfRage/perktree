44a9d59d662fed4dbacc9aa41f7bf5f8922f2b56
Pavle Portic <pavle.portic@tilda.center>
2019-03-26 04:34:02 +0100

    Update entrypoint to create user on startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,6 +9,12 @@
 sleep 5
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}')" | python manage.py shell
+echo " \
+	from perks.models import User; \
+	from django.contrib.auth.models import User as AuthUser; \
+	base_user = AuthUser.objects.get(id=1) \
+	user = User(base_user=base_user) \
+	user.save()" | python manage.py shell
 
 gunicorn -w 4 --bind 0.0.0.0:80 perktree.wsgi:application
 
