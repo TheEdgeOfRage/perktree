1ae5109489d905e3fd9c6881ad4124b002e1295c
Pavle Portic <pavle.portic@tilda.center>
2019-03-26 04:43:28 +0100

    Remove backslashes from entrypoint due to issues

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,15 +8,21 @@
 
 sleep 5
 python manage.py migrate
+
 echo "
-from perks.models import User; \
-from django.contrib.auth.models import User as AuthUser; \
-if(not AuthUser.objects.filter(username='${DJANGO_ADMIN_USER}').count()): \
-	AuthUser.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}') \
-if(not User.objects.filter(base_user__username='${DJANGO_ADMIN_USER}').count()): \
-	base_user = AuthUser.objects.get(username='${DJANGO_ADMIN_USER}') \
-	user = User(base_user=base_user) \
-	user.save()" | python manage.py shell
+from django.contrib.auth.models import User as AuthUser;
+if not AuthUser.objects.filter(username='${DJANGO_ADMIN_USER}').count() :
+	AuthUser.objects.create_superuser('${DJANGO_ADMIN_USER}', '${DJANGO_ADMIN_MAIL}', '${DJANGO_ADMIN_PASS}')
+" | python manage.py shell
+
+echo "
+from perks.models import User;
+from django.contrib.auth.models import User as AuthUser;
+if not User.objects.filter(base_user__username='${DJANGO_ADMIN_USER}').count() :
+	base_user = AuthUser.objects.get(username='${DJANGO_ADMIN_USER}')
+	user = User(base_user=base_user)
+	user.save()
+" | python manage.py shell
 
 gunicorn -w 4 --bind 0.0.0.0:80 perktree.wsgi:application
 
